23743a46f6cfec36c069d0ebb82200ae2385b365
auwelord <apalancher@gmail.com>
2024-09-13 09:45:33 +0200

    fix: test

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(cors());
 
 app.get('/proxy', async (req, res) => {
 
+    console.log(req.query)
     const url = req.query.url;
+
+console.log(url)
+
     try {
         const response = await fetch(url);
         const data = await response.buffer();
