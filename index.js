86532315505d8255d07921ad5036821749e94146
auwelord <apalancher@gmail.com>
2024-09-16 19:05:02 +0200

    fix: cors

--- index.js
+++ index.js
@@ -2,20 +2,45 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
-
 const app = express();
-app.use(cors());
 
-app.get('/proxy', async (req, res) => {
+const allowedOrigins = ['http://localhost:5173', 'http://192.168.1.160:5173', 'https://realtered.onrender.com'];
 
+const corsOptions = 
+{
+    origin: function (origin, callback) 
+    {
+        // Check if the request's origin is in the allowedOrigins array
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) 
+        {
+            callback(null, true);
+        }
+        else
+        {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,  // Enable credentials (cookies)
+}
+
+
+app.use(cors(corsOptions));
+
+app.get('/proxy', async (req, res) => 
+{
     const url = req.query.url;
+
+    console.log(url)
 
     try {
         const response = await fetch(url);
         const data = await response.buffer();
         res.set('Content-Type', response.headers.get('content-type'));
         res.send(data);
-    } catch (error) {
+    } 
+    catch (error) 
+    {
+        console.error("Error fetching image")
         console.error(error)
         res.status(500).send('Error fetching the image.');
     }
